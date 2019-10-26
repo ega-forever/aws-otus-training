3a619b74d05064e499ed48e02de18b3482cb6528
egor <zyev.egor@rambler.ru>
2019-10-26 20:33:31 +0300

    *sqs

--- queues/activemq/config/index.js
+++ queues/activemq/config/index.js
@@ -2,10 +2,11 @@ require('dotenv').config();
 
 module.exports = {
   connection: {
-    host: process.env.HOST || 'localhost',
-    port: process.env.PORT ? parseInt(process.env.PORT, 10) : 5672,
-    username: process.env.USERNAME,
-    password: process.env.PASSWORD
+    host: process.env.QUEUE_HOST || 'localhost',
+    port: process.env.QUEUE_PORT ? parseInt(process.env.QUEUE_PORT, 10) : 5672,
+    username: process.env.QUEUE_USERNAME,
+    password: process.env.QUEUE_PASSWORD,
+    transport: process.env.QUEUE_TRANSPORT
   },
   queue: process.env.QUEUE || 'example'
 
