3f088a62fe769affbd672090ea4de858be5f4bcb
egor <zyev.egor@rambler.ru>
2019-10-26 09:18:45 +0300

    *activemq *activemq_stomp

---
+++ activemq_stomp/producer/index.js
@@ -0,0 +1,55 @@
+const config = require('../config'),
+  stompit = require('stompit');
+
+const init = async () => {
+
+  const connectOptions = {
+    host: config.connection.host,
+    port: config.connection.port,
+    connectHeaders: {
+      host: '/',
+      login: config.connection.username,
+      passcode: config.connection.password,
+      'heart-beat': '5000,5000'
+    }
+  };
+
+  const client = await new Promise((res, rej) =>
+    stompit.connect(connectOptions, (error, client) => error ? rej(error) : res(client)
+    )
+  );
+
+
+  const sendHeaders = {
+    'destination': `/queue/${config.queue}`,
+    'content-type': 'text/plain'
+  };
+
+
+  setInterval(() => {
+    const frame = client.send(sendHeaders);
+    frame.write(JSON.stringify({
+        ping: Date.now()
+      })
+    );
+    frame.end();
+  }, 1000);
+
+  /*  const connection = rhea.connect(config.connection);
+    connection.open_sender(config.queue);
+
+    const ctx = await new Promise(res =>
+      rhea.once('sendable', res)
+    );
+
+    setInterval(() => {
+      ctx.sender.send({
+        body: JSON.stringify({
+          ping: Date.now()
+        })
+      });
+    }, 1000);*/
+
+};
+
+module.exports = init();
